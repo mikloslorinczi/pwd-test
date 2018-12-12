02fba4c79661a09b6723a8506bdc9fc6335a9b9c
mikloslorinczi <miklos.lorinczi@bitrise.io>
2018-12-12 15:51:11 +0100

    main.go added

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	fmt.Println("All env vars:")
+	fmt.Println("=========================")
+	fmt.Println(strings.Join(os.Environ(), "\n---\n"))
+	fmt.Println("=========================")
+
+	cmdLog, err := exec.Command("pwd").CombinedOutput()
+	if err != nil {
+		fmt.Printf("Failed to run command, error: %#v | output: %s", err, cmdLog)
+		os.Exit(1)
+	}
+
+	fmt.Println()
+	fmt.Printf("'pwd' command output: %s", cmdLog)
+	fmt.Println()
+
+	os.Exit(0)
+}
